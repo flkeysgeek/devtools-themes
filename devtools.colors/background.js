d5f0cf1d40506393162938887c261007ec592068
flkeysgeek <flkeysgeek@gmail.com>
2017-05-30 12:21:41 -0400

    cleanup

---
+++ devtools.colors/background.js
@@ -0,0 +1,36 @@
+window.g = {}
+//______________________________________________________________________________________________
+function getTargets() {
+/**
+ *
+**/
+    chrome.debugger.getTargets(function(targetList)
+    {
+        g.targetList = targetList;    
+    })
+}
+//______________________________________________________________________________________________
+function getPanel(winId, name) {
+/**
+ *
+**/
+    var id = (winId == -2) ? 'current' : id;
+    name = name || id;
+    chrome.windows.get(winId, {windowTypes: ['panel']}, function(win)
+    {
+        g[name] = win;    
+    })
+}
+/*--------------------------------------------------------------------------------------------------
+Dreamweaver LINT global references and defined variables not used here {
+--------------------------------------------------------------------------------------------------*/
+/*global 
+    chrome,
+    g:true
+*/
+var e;			//global used for try/catch
+//. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .}
+(function() {[	//global variables and functions defined but not used
+    getTargets,
+    getPanel,
+e, g]});
