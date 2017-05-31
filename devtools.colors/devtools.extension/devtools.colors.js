cdf6b5e7db8da0bbdf2a5e1e65ee06a4912ab0d6
flkeysgeek <flkeysgeek@gmail.com>
2017-05-31 12:45:16 -0400

    more dir reorg

--- devtools.colors/devtools.extension/devtools.colors.js
+++ devtools.colors/devtools.extension/devtools.colors.js
@@ -7,15 +7,21 @@ Access to same subset of the extension and runtime APIs as content script
 CREDITS:
     colors*.png https://besserwerker.com/
     https://www.iconfinder.com/icons/683988/alphabet_color_google_media_network_social_icon#size=128
+
+    NOT USED (cloned from coffeeScript): panel space too scarse
+    
+
+    chrome.devtools.panels.create(
+        "Colors",
+        "colors-16.png",
+        "colors.html",
+        function cb(panel) {
+            panel.onShown.addListener(function(win){ win.focus(); });
+        }
+    );
 --------------------------------------------------------------------------------------------------*/
-chrome.devtools.panels.create(
-    "Colors",
-    "colors-16.png",
-    "colors.html",
-    function cb(panel) {
-        panel.onShown.addListener(function(win){ win.focus(); });
-    }
-);
+
+
 /*--------------------------------------------------------------------------------------------------
 Dreamweaver LINT global references and defined variables not used here {
 --------------------------------------------------------------------------------------------------*/
@@ -25,5 +31,5 @@ Dreamweaver LINT global references and defined variables not used here {
 var e;			//global used for try/catch
 //. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .}
 (function() {[	//global variables and functions defined but not used
-
+    chrome,
 e]});
